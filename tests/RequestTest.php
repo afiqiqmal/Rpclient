384935657fa1b90ad20cedad2a711f064d233b20
Hafiq <hafiqiqmal93@gmail.com>
2020-02-03 16:26:34 +0800

    Setup test

---
+++ tests/RequestTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * RequestTest.php
+ * to test function in Request class
+ */
+class RequestTest extends TestCase
+{
+
+}
